a5902697f5dd010db478bff4bd279a0e4dda08f6
Rahman Syeedur <rahman.syeedur@valmetpartners.com>
2024-08-14 04:05:05 -0500

    Added get post logic

---
+++ MinimalApi/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApi {
+    public class Coupon {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Data;
+using System.Collections.Generic;
+
 namespace MinimalApi {
     public class Program {
         public static void Main(string[] args) {
@@ -20,10 +24,37 @@ namespace MinimalApi {
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-            
+
+            app.MapGet("/api/coupon", () => {
+                return Results.Ok(CouponStore.CouponList);
+            }).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
+
+            app.MapGet("/api/coupon/{id:int}", (int id) => {
+                return Results.Ok(CouponStore.CouponList.FirstOrDefault(x=>x.Id==id));
+            }).WithName("GetCoupon").Produces<Coupon>(200);
+
+            app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
+                if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name) ) {
+                    return Results.BadRequest("Invalid Id or coupon name");
+                }
+                if (CouponStore.CouponList.FirstOrDefault(x=>x.Name.ToLower() == coupon.Name.ToLower()) != null) {
+                    return Results.BadRequest("Coupon name is already exists");
+                }
+                coupon.Id = CouponStore.CouponList.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+                CouponStore.CouponList.Add(coupon);
+                return Results.CreatedAtRoute($"GetCoupon",new { Id = coupon.Id}, coupon);
+                //return Results.Created($"/api/coupon/{coupon.Id}", coupon);
+            }).WithName("PostCoupon").Accepts<Coupon>("application/json").Produces<Coupon>(201).Produces(400);
+
+            app.MapPut("/api/coupon", () => {
+                
+            }).WithName("UpdateCoupon");
+
+            app.MapDelete("/api/coupon/{id:int}", (int id) => {
+
+            }).WithName("DeleteCoupon");
 
             app.Run();
         }
